e0037c3fee460ba3e2bd9ed9bba6641e63fcbee1
Magdalena Tarka <mxtarka@gmail.com>
2021-08-23 15:33:53 +0200

    Implement connection with data base by mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -28,8 +29,16 @@ app.get('*', (req, res) => {
 });
 
 app.use((req, res) => {
-    res.status(404).send( { message: 'Not found....' } );
-  })
+  res.status(404).send( { message: 'Not found....' } );
+})
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
