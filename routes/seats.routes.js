d240fc6f7301eeb2031fdcb9f166fa0851744ae2
Magdalena Tarka <mxtarka@gmail.com>
2021-08-23 18:58:39 +0200

    Seperate functionality from routing to external files (controllers)

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,66 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Seat = require('../models/seat.model');
+const SeatController = require('../controllers/seats.controller');
 
-router.get('/seats', async (req, res) => {
-  try {
-    res.json(await Seat.find());
-  }
-  catch(err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.get('/seats', SeatController.getAll);
 
-router.get('/seats/:id', async (req, res) => {
-  try {
-    const seat = await Seat.findById(req.params.id);
-    if(!seat) res.status(404).json({ message: 'Not found' });
-    else res.json(seat);
-  }
-  catch(err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.get('/seats/:id', SeatController.getById);
 
-router.post('/seats', async (req, res) => {
-  try {
-    const { day, seat, client, email } = req.body;
-    const newElement = new Seat({ day: day, seat: seat, client: client, email: email });
-    await newElement.save();
-    res.json({ message: 'OK' });
-  }
-  catch(err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.post('/seats', SeatController.post);
 
-router.put('/seats/:id', async (req, res) => {
-  try {
-    const { day, seat, client, email } = req.body;
-    const newSeat = await Seat.findById(req.params.id);
-    if(newSeat) {
-      await Seat.updateOne({ _id: req.params.id }, { $set: { day: day, seat: seat, client: client, email: email } });
-      res.json({ message: 'OK' });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch(err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.put('/seats/:id', SeatController.update);
 
-router.delete('/seats/:id', async (req, res) => {
-  try {
-    const seat = await Seat.findById(req.params.id);
-    if(seat) {
-      await Seat.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch(err) {
-    res.status(500).json({ message: err });
-  }
-});
+router.delete('/seats/:id', SeatController.delete);
 
 module.exports = router;
